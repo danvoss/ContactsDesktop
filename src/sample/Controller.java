6ce190a751c47ef81202879dfe24047f2c20072a
Dan <danielandrewvoss@gmail.com>
2016-05-31 13:47:18 -0400

    Add add and remove methods

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,8 +2,43 @@ package sample;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    TextField textName;
+
+    @FXML
+    TextField textPhone;
+
+    @FXML
+    TextField textEmail;
+
+    @FXML
+    ListView list;
+
     ObservableList<Contact> contacts = FXCollections.observableArrayList();
 
+    public void onAdd() {
+        contacts.add(new Contact(textName.getText(), textPhone.getText(), textEmail.getText()));
+        textName.clear();
+        textPhone.clear();
+        textEmail.clear();
+    }
+
+    public void onRemove() {
+        Contact item = (Contact) list.getSelectionModel().getSelectedItem();
+        contacts.remove(item);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        list.setItems(contacts);
+    }
 }

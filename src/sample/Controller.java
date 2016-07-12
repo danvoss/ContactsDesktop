4df0a48c9edc437bc98941b1907fdefa5e5d715e
Dan <danielandrewvoss@gmail.com>
2016-07-11 20:41:00 -0400

    Uncomment to update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,6 +14,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 import java.util.Scanner;
@@ -54,31 +55,25 @@ public class Controller implements Initializable {
         list.setItems(contacts);
     }
 
-//    public ObservableList<Contact> readFile() {
-//         create an ArrayLIst of HashMaps from parse method; loop over it to create Contact objects by pulling out each
-    //     of the 3 fields
-//    }
+    public ObservableList<Contact> readfile() {
+        File f = new File("contact.json");
+            Scanner scanner = null;
+            try {
+                scanner = new Scanner(f);
+                scanner.useDelimiter("\\Z");
+                String contents = scanner.next();
+                JsonParser parser = new JsonParser();
 
-//    public ObservableList<Contact> readfile() {
-//        File f = new File("contact.json");
-//            Scanner scanner = null;
-//            try {
-//                scanner = new Scanner(f);
-//                scanner.useDelimiter("\\Z");
-//                String contents = scanner.next();
-//                JsonParser parser = new JsonParser();
-//
-//                ArrayList<HashMap<String, String>> list = parser.parse(contents);
-//                for (HashMap<String, String> h: list) {
-//                    contacts.add(new Contact(h.get("name"), h.get("phone"), h.get("email")));
-//                }
-//                return contacts;
-//            } catch (FileNotFoundException e) {
-//                e.printStackTrace();
-//                return contacts;
-//            }
-//    }
-
+                ArrayList<HashMap<String, String>> list = parser.parse(contents);
+                for (HashMap<String, String> h: list) {
+                    contacts.add(new Contact(h.get("name"), h.get("phone"), h.get("email")));
+                }
+                return contacts;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return contacts;
+            }
+    }
 
     public void writeFile(ObservableList<Contact> contacts) throws IOException {
         File f = new File("contact.json");
